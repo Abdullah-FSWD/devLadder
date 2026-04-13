711574a23218002fb71f8c2561836a4087138078
Abdullah Ansari <93380704+Abdullah-FSWD@users.noreply.github.com>
2026-04-13 14:14:08 +0530

    Update sectionController.js

--- server/src/controllers/sectionController.js
+++ server/src/controllers/sectionController.js
@@ -1,28 +1,18 @@
 const sectionService = require("../services/sectionService");
+const asyncHandler = require("../utils/asyncHandler");
 
-async function getSectionDetail(req, res, next) {
-  try {
-    const section = await sectionService.getSectionDetail(
-      req.params.id,
-      req.user._id
-    );
-    res.json({ success: true, data: section });
-  } catch (err) {
-    next(err);
-  }
-}
+const getSectionDetail = asyncHandler(async (req, res) => {
+  const section = await sectionService.getSectionDetail(req.params.id, req.user._id);
+  res.json({ success: true, data: section });
+});
 
-async function getSectionsForTrack(req, res, next) {
-  try {
-    const sections = await sectionService.getSectionsWithLockStatus(
-      req.params.trackId,
-      req.user.experienceLevel,
-      req.user._id
-    );
-    res.json({ success: true, data: sections });
-  } catch (err) {
-    next(err);
-  }
-}
+const getSectionsForTrack = asyncHandler(async (req, res) => {
+  const sections = await sectionService.getSectionsWithLockStatus(
+    req.params.trackId,
+    req.user.experienceLevel,
+    req.user._id
+  );
+  res.json({ success: true, data: sections });
+});
 
 module.exports = { getSectionDetail, getSectionsForTrack };
