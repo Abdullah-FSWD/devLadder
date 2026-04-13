6d75d9cd4da3592101ff4df5ce9a96b1048345df
Abdullah Ansari <93380704+Abdullah-FSWD@users.noreply.github.com>
2026-04-13 14:22:22 +0530

    Update sections.js

--- server/src/routes/sections.js
+++ server/src/routes/sections.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const { getSectionDetail, getSectionsForTrack } = require("../controllers/sectionController");
+const { validateObjectId } = require("../middleware/validate");
 const { authenticate, requireOnboarding } = require("../middleware/auth");
 
 const router = Router();
 
-router.get("/track/:trackId", authenticate, requireOnboarding, getSectionsForTrack);
-router.get("/:id", authenticate, requireOnboarding, getSectionDetail);
+router.get("/track/:trackId", authenticate, requireOnboarding, validateObjectId("trackId"), getSectionsForTrack);
+router.get("/:id", authenticate, requireOnboarding, validateObjectId("id"), getSectionDetail);
 
 module.exports = router;
